ab295d0dcffb96f4b4c3043298f39d89070a9522
MauricioVargasB <41761276+MauricioVargasB@users.noreply.github.com>
2019-01-31 23:06:09 -0500

    Create biseccion.cpp

---
+++ biseccion.cpp
@@ -0,0 +1,36 @@
+#include<iostream> 
+#include<math.h> 
+using namespace std; 
+
+const double EULER = 2.71828182845904523536;
+const double PI = 3.14159265358979323846;
+
+double evaluar(double valor){
+    double exp=pow(EULER,valor);
+    double res=(valor*exp)-PI;
+    return res;
+}
+double biseccion(double a,double b,int MAXIT,double TOL,double EX){ 
+    int cont=1; 
+    double c; 
+    double fc; 
+    while(cont<MAXIT){ 
+        c=(a+b)/2; 
+        fc=F(c); 
+        if(abs(b-a)<TOL) 
+            return c; 
+        if(abs(fc)<EX) 
+            return c; 
+        if(F(a)*fc<0) 
+            b=c; 
+        if(fc*F(b)<0) 
+            a=c; 
+        cont=cont+1;
+    } 
+    return c;
+} 
+
+int main (int argc, char *argv[]) { 
+    cout<<biseccion(0,2,100,0.0005,0.00005); 
+    return 0; 
+} 
